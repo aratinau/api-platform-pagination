2940892563760fa0f9c8a99c0f86dbfe794a5fcb
Aymeric Ratinaud <aymeric.ratinaud@sfr.fr>
2021-07-02 10:29:48 +0200

    project ready

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,6 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];
